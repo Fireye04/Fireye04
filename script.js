76e3022e8793a06be1dd4d7262255d29e779fa45
Fireye <codekai16@gmail.com>
2024-11-25 16:30:42 -0700

    persist easter egg

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 window.onload = function () {
-    var a = document.getElementById("they");
 
     const b = document.querySelector('body');
-    let set = false;
-
+    
     initGuestbook();
 
-    a.onclick = function () {
-        if (!set) {
-            alert(
-                "Hey look you found an easter egg! " +
-                "You can click the same spot to spare your eyes."
-            );
-
-            b.style.backgroundImage = 'url("./Assets/rainbow.svg")';
-            b.style.backgroundSize = "100% 600px";
-            b.style.animation = "bgScroll 5s linear infinite";
-
-            set = true;
-        } else {
-            b.style.background = "";
-            b.style.animation = "";
-
-            set = false;
-        }
+    let set = sessionStorage.getItem("rainbow");
+    if (set === null) {
+      set = "false"
+      sessionStorage.setItem("rainbow", "false");
+    } 
 
 
-        return false;
-    }
+    if (set === "true"){
+        b.style.backgroundImage = 'url("./Assets/rainbow.svg")';
+        b.style.backgroundSize = "100% 600px";
+        b.style.animation = "bgScroll 5s linear infinite";
+
+    } else {
+      b.style.background = "";
+      b.style.animation = "";
+  }
+
+    
 }
 
 
